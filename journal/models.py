968b7896a7113bab0ac8a36523afabf09cc3b2cc
Lia Kim <j.liakim23@gmail.com>
2023-11-06 21:28:22 -0500

    changed mood and photo field

--- journal/models.py
+++ journal/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class Journal(models.Model):
-    mood = models.CharField(max_length=100)
+    MOOD_CHOICES = [
+        ('Happy', 'Happy'),
+        ('Excited', 'Excited'),
+        ('Sad', 'Sad'),
+        ('Angry', 'Angry'),
+        ('Irritated', 'Irritated'),
+        ('Anxious', 'Anxious'),
+        ('Stressed', 'Stressed'),
+    ]
+
+    mood = models.CharField(max_length=100, choices=MOOD_CHOICES)
     activities = models.TextField(blank=True, null=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True)
+    photo = models.URLField(blank=True, null=True)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
