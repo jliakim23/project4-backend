a88bde1aedae8768c703e25e5baedf4278b29eab
Lia Kim <j.liakim23@gmail.com>
2023-11-08 20:04:53 -0500

    .

--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Todo
+from .models import Journal
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import JournalSerializer
@@ -9,7 +9,7 @@ from .serializers import JournalSerializer
 
 class JournalViewSet(viewsets.ModelViewSet):
     ## The Main Query for the index route
-    queryset = Todo.objects.all()
+    queryset = Journal.objects.all()
     # The serializer class for serializing output
     serializer_class = JournalSerializer
     # optional permission class set permission level
