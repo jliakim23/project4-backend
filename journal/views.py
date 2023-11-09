05fa9293a559d4b61607d5bddf29c2a6efb6f270
Lia Kim <j.liakim23@gmail.com>
2023-11-08 19:53:46 -0500

    create and update routes

--- journal/views.py
+++ journal/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+
+# Create your views here.
+from .models import Todo
 from rest_framework import viewsets
-from .models import Journal
+from rest_framework import permissions
 from .serializers import JournalSerializer
 
-class JournalViewSet(viewsets.ModelViewSet):
-    queryset = Journal.objects.all()
-    serializer_class = JournalSerializer
 
-    def get_object(self):
-        # This method is used to retrieve the specific journal object
-        # Override if necessary to handle lookup differently
-        return super().get_object()
+class JournalViewSet(viewsets.ModelViewSet):
+    ## The Main Query for the index route
+    queryset = Todo.objects.all()
+    # The serializer class for serializing output
+    serializer_class = JournalSerializer
+    # optional permission class set permission level
+    permission_classes = [permissions.AllowAny] #Coule be [permissions.IsAuthenticated]
